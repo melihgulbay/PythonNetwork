f8b4a41e5e23431e88875b4eb0eb89268204b960
melihgulbay <168998569+melihgulbay@users.noreply.github.com>
2024-12-14 10:26:11 +0300

    Delete Yedek3/ThesisPy directory

--- Yedek3/ThesisPy/pythonProject/packet.py
+++
@@ -1,7 +0,0 @@
-# packet.py
-class Packet:
-    def __init__(self, id, source, destination, creation_time):
-        self.id = id
-        self.source = source
-        self.destination = destination
-        self.creation_time = creation_time
